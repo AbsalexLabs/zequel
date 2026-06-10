88803fdbceb38b86e5cd7e3ce31fceac29ff122f
v0agent <it+v0agent@vercel.com>
2026-06-10 11:40:10 +0000

    chore(ui): finalize @zequel/ui package (imports, barrel, manifest, globals)

--- packages/ui/src/components/theme-toggle.tsx
+++ packages/ui/src/components/theme-toggle.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useTheme } from 'next-themes'
-import { Button } from '@/components/ui/button'
+import { Button } from './button'
 import { Sun, Moon } from 'lucide-react'
 import { useEffect, useState } from 'react'
 
